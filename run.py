6b8661f4f082367f139c44119c631807ff81b11e
Joe Turner <joe@oampo.co.uk>
2014-10-01 10:55:21 +0100

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 from chords import app
 
 def run():
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
